745306d648b6c2257d833d3f7917a62d9a79bb8c
AudreyMistris <57502743+AudreyMistris@users.noreply.github.com>
2020-07-19 14:13:04 -0300

    Update game-scene.js

--- js/game-scene.js
+++ js/game-scene.js
@@ -12,9 +12,11 @@ export default class GameScene extends Phaser.Scene {
     }
 
     create() {
-        const LARGURA_JOGO = this.sys.canvas.width;
-        const ALTURA_JOGO = this.sys.canvas.height;
-        this.add.image(LARGURA_JOGO, ALTURA_JOGO, 'room');
+        // const LARGURA_JOGO = this.sys.canvas.width;
+        // const ALTURA_JOGO = this.sys.canvas.height;
+        // this.add.image(LARGURA_JOGO, ALTURA_JOGO, 'room');
+        const IMG_BACKGROUND = this.add.image(0, 0, 'room');
+        IMG_BACKGROUND.setOrigin(0, 0);
         
         this.player = new Player(this);
 
