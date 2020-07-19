23b6a19e2d022001ca11cb75edcde0c30ae2abdf
AudreyMistris <57502743+AudreyMistris@users.noreply.github.com>
2020-07-19 13:19:17 -0300

    Update loading-scene.js

--- js/loading-scene.js
+++ js/loading-scene.js
@@ -6,10 +6,16 @@ export default class LoadingScene extends Phaser.Scene {
     }
 
     preload() {
-        this.load.image();
-        this.load.music();
-        this.load.sound();
-        this.load.spritesheet();
+        this.load.on('complete', () => {
+            this.scene.start('GameScene');
+        });
+        
+        this.load.image('room', 'lenda-do-heroi.png');
+        this.load.image('floor', 'floor.png');
+        this.load.image('platform', 'platform.png');
+        // this.load.music();
+        // this.load.sound();
+        this.load.spritesheet('sprite', 'eskimo_girl.png', { frameWidth: 24, frameHeight: 24 });
     }
 
     create() {
