542432c8f59b9ea87a1ee27945cf503f64b3c0b3
AudreyMistris <57502743+AudreyMistris@users.noreply.github.com>
2020-07-19 03:38:01 -0300

    Update game-scene.js

--- js/game-scene.js
+++ js/game-scene.js
@@ -1,3 +1,5 @@
+import Player from "./player.js";
+
 export default class GameScene extends Phaser.Scene {
     constructor() {
         super({
@@ -10,10 +12,35 @@ export default class GameScene extends Phaser.Scene {
     }
 
     create() {
+        this.player = new Player(this);
 
+        this.teclas = this.input.keyboard.createCursorKeys();
     }
 
     update() {
+        const PLAYER = this.player.sprite;
+        
+        if (this.teclas.left.isDown) {
+            PLAYER.setVelocityX(-160);
+            PLAYER.setFlip(true, false)
+            PLAYER.anims.play('left', true);
 
+        } else if (this.teclas.right.isDown) {
+            PLAYER.setVelocityX(160);
+            PLAYER.setFlip(false, false)
+            PLAYER.anims.play('right', true);
+
+        } else {
+            PLAYER.setVelocityX(0);
+            
+            if (PLAYER.body.touching.down) {
+                PLAYER.anims.play('idle');
+            }
+        }
+        
+        if (this.teclas.up.isDown && PLAYER.body.touching.down) {
+            PLAYER.setVelocityY(-100);
+            PLAYER.anims.play('jump')
+        }
     }
 }
