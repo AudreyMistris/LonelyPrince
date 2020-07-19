9d747c99fbc7adae93ba6bb602e357663607d18a
AudreyMistris <57502743+AudreyMistris@users.noreply.github.com>
2020-07-19 14:53:23 -0300

    Update loading-scene.js

--- js/loading-scene.js
+++ js/loading-scene.js
@@ -6,6 +6,18 @@ export default class LoadingScene extends Phaser.Scene {
     }
 
     preload() {
+        const LARGURA_JOGO = this.sys.canvas.width;
+        const PROGRESS_BAR = this.add.graphics();
+        const LARGURA_BARRA = 0.8 * LARGURA_JOGO;
+        
+        this.load.on('progress', (value) => {
+            PROGRESS_BAR.clear();
+            PROGRESS_BAR.fillStyle(0xFFFFFF, 1);
+            PROGRESS_BAR.fillRect((LARGURA_JOGO - LARGURA_BARRA) / 2, this.sys.game.config.height / 2, LARGURA_BARRA * value, 20);
+            PROGRESS_BAR.lineStyle(4, 0x00FFFF, 1);
+            PROGRESS_BAR.strokeRect((LARGURA_JOGO - LARGURA_BARRA) / 2, this.sys.game.config.height / 2, LARGURA_BARRA, 20);
+        });
+        
         this.load.on('complete', () => {
             this.scene.start('GameScene');
         });
