50d77c25a1ddf93323525a15c5dd7031740f99b4
AudreyMistris <57502743+AudreyMistris@users.noreply.github.com>
2020-07-20 16:40:36 -0300

    Update player.js

--- js/player.js
+++ js/player.js
@@ -2,35 +2,36 @@ export default class Player {
     constructor(scene) {
         this.scene = scene;
         this.sprite = scene.physics.add.sprite(10, 100, 'sprite');
-        this.sprite.body.setSize(59, 118);
-        this.sprite.setScale(0.5);
+        this.sprite.body.setSize(39, 50);
+        // this.sprite.setScale(0.5);
+        // this.sprite.setBounce(0.2);
         this.sprite.setCollideWorldBounds(true);
         
         scene.anims.create({
             key: 'idle',
-            frames: scene.anims.generateFrameNumbers('sprite', { start: 0, end: 3 }),
-            frameRate: 10,
+            frames: scene.anims.generateFrameNumbers('sprite', { start: 12, end: 23 }),
+            frameRate: 15,
             repeat: -1
         });
 
         scene.anims.create({
             key: 'left',
-            frames: scene.anims.generateFrameNumbers('sprite', { start: 8, end: 11 }),
-            frameRate: 10,
+            frames: scene.anims.generateFrameNumbers('sprite', { start: 0, end: 11 }),
+            frameRate: 15,
             repeat: -1
         });
 
         scene.anims.create({
             key: 'right',
-            frames: scene.anims.generateFrameNumbers('sprite', { start: 8, end: 11 }),
-            frameRate: 10,
+            frames: scene.anims.generateFrameNumbers('sprite', { start: 0, end: 11 }),
+            frameRate: 15,
             repeat: -1
         });
 
         scene.anims.create({
             key: 'jump',
-            frames: scene.anims.generateFrameNumbers('sprite', { start: 12, end: 15 }),
-            frameRate: 10,
+            frames: scene.anims.generateFrameNumbers('sprite', { start: 24, end: 35 }),
+            frameRate: 15,
             repeat: -1
         });
     }
